e2e0bc5f4ab8f4313570105fef7e16c878ae2af0
ang <15222059594@163.com>
2021-08-12 21:37:21 +0800

    路由懒加载、魔法注释

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-// 引入路由中需要使用的组件功能
-import Login from '@/views/login/index.vue'
-import Layout from '@/views/layout/index.vue'
-import Home from '@/views/home/index.vue'
-// import Role from '@/views/role/index.vue'
-
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/login',
     name: 'login',
-    component: Login
+    component: () => import(/* webpackChunkName: 'Login' */ '@/views/login/index.vue')
   },
   {
     path: '/',
-    component: Layout,
+    component: () => import(/* webpackChunkName: 'layout' */ '@/views/layout/index.vue'),
     children: [
       {
         path: '',
         name: 'home',
-        component: Home
+        component: () => import(/* webpackChunkName: 'home' */ '@/views/home/index.vue')
       }
     ]
   }
