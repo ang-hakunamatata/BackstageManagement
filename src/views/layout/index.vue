e522a755e64c05594b5a1df0d95063aacba52a48
ang <15222059594@163.com>
2021-08-13 21:51:32 +0800

    布局

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,10 +1,35 @@
 <template>
-  <!-- 设置子路由出口 -->
-  <router-view></router-view>
+
+  <el-container>
+    <el-aside width="200px">
+      <!-- 侧边栏组件 -->
+      <app-aside></app-aside>
+      <!-- <AppAside /> -->
+    </el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>
+          <!-- 设置子路由出口 -->
+          <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
+// 引入侧边栏
+import AppAside from './components/AppAside.vue'
+
+export default {
+  name: 'Layout',
+  components: {
+    AppAside
+  }
+}
 </script>
 
 <style lang="scss" scoped>
+.el-container {
+  height: 100vh;
+}
 </style>
