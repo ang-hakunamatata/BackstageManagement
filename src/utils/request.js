0ec6a7dd599d59e041236a05b7a5ed50aa2e6c9e
ang <15222059594@163.com>
2021-08-15 10:50:09 +0800

    接口请求拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,4 +5,19 @@ const request = axios.create({
   timeout: 2000
 })
 
+function getBaseURL (url) {
+  if (url.startsWith('/boss')) {
+    return 'http://eduboss.lagou.com'
+  } else {
+    return 'http://edufront.lagou.com'
+  }
+}
+
+// 请求拦截器
+request.interceptors.request.use((config) => {
+  // 判断 config.url 的前缀，来进行请求 baseURL 的设置
+  config.baseURL = getBaseURL(config.url)
+  return config
+})
+
 export default request
