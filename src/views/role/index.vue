6fabf861c8e68f7aff2ec06d0281b9ecdf76e467
ang <15222059594@163.com>
2021-08-22 11:27:22 +0800

    角色管理-分析与准备工作

---
+++ src/views/role/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="role">
+    <role-list></role-list>
+  </div>
+</template>
+
+<script>
+import RoleList from './compoents/List.vue'
+
+export default {
+  name: 'role',
+  components: {
+    RoleList
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
