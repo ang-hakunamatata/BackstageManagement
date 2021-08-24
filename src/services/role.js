b445ca21302473132ce5e554f281633d63ba5030
ang <15222059594@163.com>
2021-08-24 10:29:15 +0800

    用户管理-用户拥有菜单列表

---
+++ src/services/role.js
@@ -0,0 +1,63 @@
+import request from '@/utils/request'
+
+// 获取角色
+export const getRoles = data => {
+  return request({
+    method: 'POST',
+    url: '/boss/role/getRolePages',
+    data
+  })
+}
+
+// 删除角色
+export const deleteRole = id => {
+  return request({
+    method: 'DELETE',
+    url: `/boss/role/${id}`
+  })
+}
+
+// 保存或更新角色
+export const createOrUpdate = (data) => {
+  return request({
+    method: 'POST',
+    url: '/boss/role/saveOrUpdate',
+    data
+  })
+}
+
+// 获取角色信息
+export const getRoleById = (id) => {
+  return request({
+    method: 'GET',
+    url: `/boss/role/${id}`
+  })
+}
+
+// 分级获取菜单列表
+export const getMenuNodeList = () => {
+  return request({
+    method: 'GET',
+    url: '/boss/menu/getMenuNodeList'
+  })
+}
+
+// 角色分配选定菜单项
+export const allocateRoleMenus = (data) => {
+  return request({
+    method: 'POST',
+    url: '/boss/menu/allocateRoleMenus',
+    data
+  })
+}
+
+// 获取角色拥有的菜单列表
+export const getRoleMenus = (roleId) => {
+  return request({
+    method: 'GET',
+    url: '/boss/menu/getRoleMenus',
+    params: {
+      roleId
+    }
+  })
+}
--- src/views/role/alloc-menu.vue
+++ src/views/role/alloc-menu.vue
@@ -8,6 +8,7 @@
           :props="defaultProps"
           show-checkbox
           default-expand-all
+          :default-checked-keys="checkedKeys"
         ></el-tree>
         <div style="margin: 20px;">
           <el-button
@@ -23,7 +24,7 @@
 </template>
 
 <script>
-import { getMenuNodeList, allocateRoleMenus } from '@/services/role.js'
+import { getMenuNodeList, allocateRoleMenus, getRoleMenus } from '@/services/role.js'
 
 export default {
   name: 'allocMenu',
@@ -33,7 +34,10 @@ export default {
     }
   },
   created () {
+    // 分级获取菜单列表
     this.loadMenuNodeList()
+    // 获取角色拥有的菜单列表
+    this.loadRoleMenus()
   },
   data () {
     return {
@@ -45,16 +49,45 @@ export default {
         label: 'name'
       },
       // 选定菜单项 Id 信息
-      menuIdList: []
+      menuIdList: [],
+      // 用户选中的菜单信息
+      checkedKeys: []
     }
   },
   methods: {
     // 清空选定菜单项
-    onReset () {},
+    onReset () {
+      this.$refs['menu-tree'].setCheckedKeys([])
+    },
+    // 获取用户选择菜单列表展示
+    getCheckedKey (menus) {
+      menus.forEach(menu => {
+        // 未选中，结束
+        if (!menu.selected) {
+          return
+        }
+        // 如果拥有子节点不进行操作，递归子节点
+        if (menu.subMenuList) {
+          return this.getCheckedKey(menu.subMenuList)
+        }
+        // 可以进行处理的子节点
+        // this.checkedKeys.push(menu.id)
+        // console.log(this.checkedKeys)
+        this.checkedKeys = [...this.checkedKeys, menu.id]
+        console.log(this.checkedKeys)
+      })
+    },
+    async loadRoleMenus () {
+      const { data } = await getRoleMenus(this.roleId)
+      // console.log(data)
+      if (data.code === '000000') {
+        this.getCheckedKey(data.data)
+      }
+    },
     // 保存选定菜单项
     async onSave () {
-    //   console.log(this.$refs['menu-tree'].getCheckedKeys())
-    //   console.log(this.$refs['menu-tree'].getCheckedNodes())
+      // console.log(this.$refs['menu-tree'].getCheckedKeys())
+      // console.log(this.$refs['menu-tree'].getCheckedNodes())
       // 发送请求，传递角色 ID 与 选中的菜单项 ID
       const { data } = await allocateRoleMenus({
         roleId: this.roleId,
